728a99163af7aca2dfc50bce92e66abf30d867d1
GvMazzon25 <gvmazzon25@gmail.com>
2024-01-01 17:18:21 +0100

    Data analizator start

---
+++ AnalisiVendite/analisi_vendite.py
@@ -0,0 +1,10 @@
+#Immagina di lavorare per un'azienda che vende prodotti online.
+# Hai ricevuto un file CSV contenente i dati delle vendite degli ultimi mesi.
+# Il tuo compito è analizzare questi dati per fornire informazioni utili all'azienda
+
+
+def main():
+    print('csv')
+
+
+main()
