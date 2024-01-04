3ce6951dd8c85e413ba52a283d832ae846f02c7e
GvMazzon25 <gvmazzon25@gmail.com>
2024-01-04 23:28:52 +0100

    LOG analizer

---
+++ FileLog/file_log.py
@@ -0,0 +1,23 @@
+#Immagina di avere un file di log di un'applicazione,
+# dove ogni riga registra un evento con una data, un'ora e un messaggio di log.
+# Il tuo compito è scrivere uno script Python per analizzare questo file di log e produrre un riassunto.
+
+
+def read_log():
+    with open('log.log', 'r') as file:
+        linee = file.readline()
+        numero_righe = len(linee)
+
+
+
+
+def controller():
+    contenuto = read_log()
+    if contenuto is None:
+        return 'Error'
+    else:
+
+
+
+def main():
+
